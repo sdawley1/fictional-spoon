6df37131832ad9f986645659c7d6459db0817b28
Sam <92101227+sdawley1@users.noreply.github.com>
2022-07-02 18:34:12 -0400

    Add files via upload

---
+++ chess_engine/temp.py
@@ -0,0 +1,6 @@
+from stockfish import Stockfish
+
+# Lo hicimos, 'we did it!'
+stockfish = Stockfish(path="/Users/samdawley/Downloads/stockfish_15_linux_x64_bmi2/stockfish_15_src/src/stockfish")
+
+print("hello")
