2a3a0b9f233425131c9da6c538c8174715b6579d
Sam <92101227+sdawley1@users.noreply.github.com>
2022-07-02 18:34:31 -0400

    Update temp.py

--- chess_engine/temp.py
+++ chess_engine/temp.py
@@ -1,6 +1,6 @@
 from stockfish import Stockfish
 
 # Lo hicimos, 'we did it!'
-stockfish = Stockfish(path="/Users/samdawley/Downloads/stockfish_15_linux_x64_bmi2/stockfish_15_src/src/stockfish")
+stockfish = Stockfish(path="/Users/USERNAME/Downloads/stockfish_15_linux_x64_bmi2/stockfish_15_src/src/stockfish")
 
 print("hello")
