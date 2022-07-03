6558e278faf6a24dca177717d5fed9b02f76a8ac
Sam <92101227+sdawley1@users.noreply.github.com>
2022-07-03 10:58:18 -0400

    Update players.py

--- chess_engine/pvp/players.py
+++ chess_engine/pvp/players.py
@@ -7,12 +7,12 @@ class Player:
         self.engine = engine
 
     def add_win(self):
-        """ Add a win to a player's record"""
+        """ Add a win to a player's record """
         self.wins += 1
         return
 
     def add_loss(self):
-        """ Add a loss to a player's record"""
+        """ Add a loss to a player's record """
         self.losses += 1
         return
 
