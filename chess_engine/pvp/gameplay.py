df367bd0d2090528402c239af267b166e47bf468
Sam <92101227+sdawley1@users.noreply.github.com>
2022-07-03 11:43:32 -0400

    Update gameplay.py

--- chess_engine/pvp/gameplay.py
+++ chess_engine/pvp/gameplay.py
@@ -50,6 +50,13 @@ def GameplayLoop(p1, p2, area):
 
         # Test termination condition
         outcome = area.outcome()
+        
+    # Print final board
+    print()
+    print("Game over!")
+    print("---------------")
+    print(area)
+    print("---------------")
 
     # Return results of the match
     return outcome.termination, outcome.winner, outcome.result()
