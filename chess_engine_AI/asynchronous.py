a00da29222a91ed741102fafe2063cdaa9d400c0
Sam <92101227+sdawley1@users.noreply.github.com>
2022-07-03 18:27:16 -0400

    Delete chess_engine_AI directory

--- chess_engine_AI/asynchronous.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-async def main():
-    print('Hello ...')
-    await asyncio.sleep(1)
-    print('... World!')
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- chess_engine_AI/temp.py
+++
@@ -1,6 +0,0 @@
-from stockfish import Stockfish
-
-# Lo hicimos, 'we did it!'
-stockfish = Stockfish(path="/Users/samdawley/Downloads/stockfish_15_linux_x64_bmi2/stockfish_15_src/src/stockfish")
-
-print("hello")
