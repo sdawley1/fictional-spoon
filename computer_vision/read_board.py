617734600cde92f57e411f88695d3ea4db05538f
ofw24 <oliverfwolff@gmail.com>
2022-07-05 20:30:37 -0400

    starting computer vision things

---
+++ computer_vision/read_board.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import imutils
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv.imread('1345.jpg',0)
+template = cv.imread('sampleqr.jpg',0)
+resized = imutils.resize(template, width = int(img.shape[1] * 0.1))
+w, h = template.shape[::-1]
+# All the 6 methods for comparison in a list
+methods = 'cv.TM_CCOEFF'
+
+method = eval(methods)
+# Apply template Matching
+res = cv.matchTemplate(img,template,method)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
+cv.rectangle(img,top_left, bottom_right, (255, 0, 0), 2)
+plt.subplot(121),plt.imshow(res,cmap = 'gray')
+plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img,cmap = 'gray')
+plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+plt.suptitle(method)
+
+print()
+
+#plt.imshow(resized)
+#plt.show()
