882d69cf58819b41fccf69aa51a38e186d2bb879
ofw24 <oliverfwolff@gmail.com>
2022-07-17 22:13:20 -0400

    outputs fen from a picture of the board, needs testing

--- computer_vision/read_board.py
+++ computer_vision/read_board.py
@@ -4,19 +4,32 @@ import imutils
 import numpy as np
 from matplotlib import pyplot as plt
 from itertools import combinations
+from pathfinding.create_random_fen import fen_from_board
 
-
-'''inputs are the picture of the board, dictionary of piece names (upper and lowercase for color)
-and template filenames. does a for loop, calls another function that outputs the square each piece
-is in. outputs 8x8 numpy array representing the current state.'''
-def read_board(board_pic_path, template_names):
-    os.chdir("./qrcodes")
+def get_square(top_left):
     file_coords = {'a': 100, 'b': 200, 'c': 300, 'd': 400,
                     'e':500, 'f': 600, 'g': 700, 'h': 800}
     rank_coords = {1: 100, 2: 200, 3: 300, 4: 400,
                     5:500, 6: 600, 7: 700, 8: 800}
+    file_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4,
+                    'e':5, 'f': 6, 'g': 7, 'h': 8}
+    
+    file_coord = round(top_left[0], -1)
+    rank_coord = round(top_left[1], -1)
 
-    for i in template_names:
+    file, _ = min(file_coords.items(), key=lambda x: abs(file_coord - x[1]))
+    rank, _ = min(rank_coords.items(), key=lambda x: abs(rank_coord - x[1]))
+
+    return(file_num[file], rank)
+
+'''inputs are the picture of the board, dictionary of piece names (upper and lowercase for color)
+and template filenames. does a for loop, calls another function that outputs the square each piece
+is in. outputs 8x8 numpy array representing the current state.'''
+def read_board(board_pic_path, template_fnames, piece_names):
+    board = np.empty((8, 8), dtype='str')
+    board[:] = ' '
+
+    for template, piece in zip(template_fnames, piece_names):
         template = cv.imread(i,0)
         template = imutils.resize(template, width = int(img.shape[1] * 0.02))
 
@@ -25,3 +38,8 @@ def read_board(board_pic_path, template_names):
         res = cv.matchTemplate(board_pic_path,template,method)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         top_left = max_loc
+
+        file, rank =  get_square(top_left)
+        board[file-1][rank-1] = piece
+    
+    return(fen_from_board(board))
