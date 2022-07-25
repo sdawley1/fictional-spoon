cc0a3c3945a1876513cdb23ef61058b9317964b3
ofw24 <oliverfwolff@gmail.com>
2022-07-25 14:18:27 -0400

    successfully tested read_board with one piece, plus extra test file w/ helpful info

--- computer_vision/read_board.py
+++ computer_vision/read_board.py
@@ -9,10 +9,10 @@ sys.path.append('../')
 from pathfinding.create_random_fen import fen_from_board
 
 def get_square(top_left):
-    file_coords = {'a': 100, 'b': 200, 'c': 300, 'd': 400,
-                    'e':500, 'f': 600, 'g': 700, 'h': 800}
-    rank_coords = {1: 100, 2: 200, 3: 300, 4: 400,
-                    5:500, 6: 600, 7: 700, 8: 800}
+    file_coords = {'a': 61, 'b': 130, 'c': 199, 'd': 268,
+                    'e': 337, 'f': 406, 'g': 475, 'h': 544}
+    rank_coords = {9-i: 70*(i-1) + 48 for i in range(1, 9)}
+
     file_num = {'a': 1, 'b': 2, 'c': 3, 'd': 4,
                     'e':5, 'f': 6, 'g': 7, 'h': 8}
     
@@ -21,6 +21,7 @@ def get_square(top_left):
 
     file, _ = min(file_coords.items(), key=lambda x: abs(file_coord - x[1]))
     rank, _ = min(rank_coords.items(), key=lambda x: abs(rank_coord - x[1]))
+    print(str(file)+str(rank))
 
     return(file_num[file], rank)
 
@@ -29,7 +30,7 @@ and template filenames. does a for loop, calls another function that outputs the
 is in. outputs 8x8 numpy array representing the current state.'''
 def read_board(board_pic_path, template_fnames, piece_names):
     board = np.empty((8, 8), dtype='str')
-    board[:] = ' '
+    board[:] = 'o'
 
     for qrcode, piece in zip(template_fnames, piece_names):
         img = cv.imread(board_pic_path,0)
@@ -41,10 +42,11 @@ def read_board(board_pic_path, template_fnames, piece_names):
         res = cv.matchTemplate(img,template,method)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         top_left = max_loc
+        #top_left = (58, 390)
         
         file, rank =  get_square(top_left)
-        board[file-1][rank-1] = piece
+        board[8-rank][file-1] = piece
     
     return(board)
 
-print(read_board('cvtest.png', ['qrcodes/empty.jpg', 'qrcodes/2345.jpg'], ['Q', 'n']))
+print(read_board('cvtest.png', ['qrcodes/empty.jpg'], ['Q']))
