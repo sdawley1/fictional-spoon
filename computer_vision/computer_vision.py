cba48fbad628fd60559e337cabe7388f7c8a78be
ofw24 <oliverfwolff@gmail.com>
2022-07-29 16:31:09 -0400

    algorithm averages top left and bottom right coords of qr codes for robustness

--- computer_vision/computer_vision.py
+++ computer_vision/computer_vision.py
@@ -8,28 +8,36 @@ from itertools import combinations
 sys.path.append('../')
 from pathfinding.create_random_fen import fen_from_board
 
-def get_square(top_left: tuple) -> tuple:
+def get_square(top_left: tuple, bottom_right: tuple) -> tuple:
     '''
     returns the square corresponding to a piece's pixel location in the board image
     :param top_left: tuple of the pixel coords of the top left of the qr code
+    :param bottom_right: tuple of the pixel coords of the bottom right of the qr code
     '''
-    #file_coords = {'a': 61, 'b': 130, 'c': 199, 'd': 268,
-    #                'e': 337, 'f': 406, 'g': 475, 'h': 544}
-    #rank_coords = {9-i: 70*(i-1) + 48 for i in range(1, 9)}
-    file_coords = {i: 105*(i-1)+20 for i in range(1, 9)}
-    rank_coords = {9-i: 103*(i-1)+10 for i in range(1, 9)}
+
+    file_coords_tl = {i: 105*(i-1)+20 for i in range(1, 9)}
+    rank_coords_tl = {9-i: 103*(i-1)+10 for i in range(1, 9)}
+
+    file_coords_br = {i: 105*(i)+20 for i in range(1, 9)}
+    rank_coords_br = {9-i: 103*(i)+10 for i in range(1, 9)}
+
+    file_coords_avg = {i: np.mean([file_coords_tl[i], file_coords_br[i]]) for i in range(1, 9)}
+    rank_coords_avg = {i: np.mean([rank_coords_tl[i], rank_coords_br[i]]) for i in range(1, 9)}
 
     file_num = {1: 'a', 2:'b', 3: 'c', 4: 'd',
                     5: 'e', 6: 'f', 7: 'g', 8: 'h'}
     
-    #file_coord = round(top_left[0], -1)
-    #rank_coord = round(top_left[1], -1)
-    file_coord = top_left[0]
-    rank_coord = top_left[1]
+    file_coord_tl = top_left[0]
+    rank_coord_tl = top_left[1]
 
-    file, _ = min(file_coords.items(), key=lambda x: abs(file_coord - x[1]))
-    rank, _ = min(rank_coords.items(), key=lambda x: abs(rank_coord - x[1]))
-    #print(str(file_num[file])+str(rank))
+    file_coord_br = bottom_right[0]
+    rank_coord_br = bottom_right[1]
+
+    file_coord_avg = np.mean([file_coord_tl, file_coord_br])
+    rank_coord_avg = np.mean([rank_coord_tl, rank_coord_br])
+
+    file, _ = min(file_coords_avg.items(), key=lambda x: abs(file_coord_avg - x[1]))
+    rank, _ = min(rank_coords_avg.items(), key=lambda x: abs(rank_coord_avg - x[1]))
 
     return(file, rank)
 
@@ -49,17 +57,20 @@ def read_board(board_pic_path: str, template_fnames: list, piece_names: list) ->
 
         template = cv.imread(qrcode,0)
         template = imutils.resize(template, width = int(img.shape[1] * 0.05))
+        w, h = template.shape[::-1]
+
         method = eval('cv.TM_CCOEFF')
         res = cv.matchTemplate(img,template,method)
         _, _, _, max_loc = cv.minMaxLoc(res)
         top_left = max_loc
-        #print(top_left, piece)
-        
-        file, rank =  get_square(top_left)
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+     
+        file, rank =  get_square(top_left, bottom_right)
         board[8-rank][file-1] = piece
     return(fen_from_board(board))
 
 #print(read_board('cvtest2.png', ['qrcodes/empty.jpg', 'qrcodes/12345.jpg', 'qrcodes/1234.jpg'], ['K', 'k', 'N']))
+
 
 def check_update(current_board: str, board_pic_path: str, template_fnames: list, piece_names: list) -> bool:
     '''
