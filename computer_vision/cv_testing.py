004b06d8455e73b312898db7c6962cce49f8fc13
ofw24 <oliverfwolff@gmail.com>
2022-07-29 16:32:24 -0400

    small testing changes to add avg of code location

--- computer_vision/cv_testing.py
+++ computer_vision/cv_testing.py
@@ -11,14 +11,16 @@ from itertools import combinations
 
 file_coords = {i: 105*(i-1)+20 for i in range(1, 9)}
 rank_coords = {9-i: 103*(i-1)+10 for i in range(1, 9)}
-print(file_coords)
-print(rank_coords)
+file_coords_br = {i: 105*(i)+20 for i in range(1, 9)}
+rank_coords_br = {9-i: 103*(i)+10 for i in range(1, 9)}
+#print(rank_coords)
+#print(rank_coords_br)
 
 img=cv.imread('cvtest2.png')
 for i in range(1, 8):
-    cv.rectangle(img, (file_coords[i], rank_coords[i]),
-        (file_coords[i+1], rank_coords[i+1]), (255, 0, 255), 2)
-#cv.rectangle(img, (file_coords[5], rank_coords[5]), (file_coords[5]+1, rank_coords[5]+5), (255, 0, 255), 2)
+    cv.rectangle(img, (file_coords_br[i], rank_coords_br[i]),
+        (file_coords_br[i+1], rank_coords_br[i+1]), (255, 0, 255), 2)
+cv.rectangle(img, (file_coords[6], rank_coords[8]), (file_coords_br[6], rank_coords_br[8]), (255, 0, 255), 2)
 #cv.rectangle(img, (20, 4), (120, 106), (255, 0, 255), 2)
 cv.imshow('img', img)
 cv.waitKey(0)
